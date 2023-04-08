a90345298c82447f9036318f6bb5035ef83e2243
isa-koseoglu <isaparta.65@gmail.com>
2023-04-08 21:35:06 +0300

    C# old examples

---
+++ SqlLiteConnectionExample/SqlLiteConnectionExample/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlLiteConnectionExample
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        SQLiteConnection conn;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SQLiteConnection("Data Source=Db/WebBayiCompany.db;Verison=3");
+            conn.Open();
+            MessageBox.Show(conn.State.ToString());
+        }
+    }
+}
